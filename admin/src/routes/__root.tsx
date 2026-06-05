23f04319c18ac7dd35f20cd079ad28509c0f318b
ronianpelagio <ronianpelagio111@gmail.com>
2026-06-05 16:25:48 +0800

    root fix

--- admin/src/routes/__root.tsx
+++ admin/src/routes/__root.tsx
@@ -72,14 +72,13 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
     meta: [
       { charSet: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { title: "Lovable App" },
-      { name: "description", content: "Lovable Generated Project" },
-      { name: "author", content: "Lovable" },
-      { property: "og:title", content: "Lovable App" },
-      { property: "og:description", content: "Lovable Generated Project" },
+      { title: "ETUrismo" },
+      { name: "description", content: "Admin of Eturismo Mobile app. This app is web based only" },
+      { name: "author", content: "Ronian Pelagio" },
+      { property: "og:title", content: "ETUrismo" },
+      { property: "og:description", content: "Admin of Eturismo Mobile app. This app is web based only" },
       { property: "og:type", content: "website" },
       { name: "twitter:card", content: "summary" },
-      { name: "twitter:site", content: "@Lovable" },
     ],
     links: [
       {
