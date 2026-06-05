1c6546553ec09aa08185758f45eb2992d7fe140d
ronianpelagio <ronianpelagio111@gmail.com>
2026-06-05 17:42:02 +0800

    fix j-package

--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-// use Vite's native tsconfig paths support via `resolve.tsconfigPaths`
 
 export default defineConfig({
   plugins: [
